ccf4462f5f2c9ec0b0318ec7f8f03eda5ccf047b
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-05-24 15:32:05 -0400

    Created working button

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import Counter from './src/components/Counter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>Hello !</Text>
+      <Button title='Just click me...' color="#841584" />
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  heading: {
+    fontSize: 38,
+    color: '#fff'
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0a',
     alignItems: 'center',
     justifyContent: 'center',
   },
