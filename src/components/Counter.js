3adf3bd405b2de3753853171fe327a960cc3882c
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-05-24 15:56:27 -0400

    add Plus Reset Minus buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,22 @@
 import { View, Text, Button, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function Counter (){
-    // const [counter, setCounter] = useState(0)
+    const [count, setCount] = useState(0)
     return (
         <View style={styles.counterContainer}>
-            <Text>You clicked the button 0 times</Text>
-            <Button title='Click to increase counter' 
-                // onPress={setCounter(counter + 1)}
-            />
+            <Text>You clicked the button {count} times</Text>
+            <Button title='-' onPress={()=> count>0?setCount(count - 1):0}/>
+            <Button title='Reset' onPress={()=>setCount(0)}/>
+            <Button title='+' onPress={()=>setCount(count + 1)}/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     counterContainer: {
-        width: '80vw',
+        width: '80%',
         backgroundColor: 'beige',
         alignItems: 'center',
         padding: 14,
